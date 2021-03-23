d349d8a25ac45ae69c920c4b1e53ba327cd6a63c
webmsgr <zapcontact000@gmail.com>
2021-03-23 12:54:45 -0700

    button

--- script.js
+++ script.js
@@ -507,4 +507,36 @@ function init_ServiceWorker() {
 		});
 	}
 }
+let deferredPrompt;
+var buttonInstall = document.getElementById("installbutton")
+
+function showInstallPromotion() {
+	buttonInstall.removeAttribute("disabled")
+}
+function hideInstallPromotion() {
+	buttonInstall.setAttribute("disabled",true)
+}
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+});
+
+buttonInstall.addEventListener('click', async () => {
+	// Hide the app provided install promotion
+	hideInstallPromotion();
+	// Show the install prompt
+	deferredPrompt.prompt();
+	// Wait for the user to respond to the prompt
+	const { outcome } = await deferredPrompt.userChoice;
+	// Optionally, send analytics event with outcome of user choice
+	console.log(`User response to the install prompt: ${outcome}`);
+	// We've used the prompt, and can't use it again, throw it away
+	deferredPrompt = null;
+  });
+
 init_ServiceWorker()

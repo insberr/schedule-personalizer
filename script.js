0e3150f8f2dd1ef47982b4b1b741bd2c7cfe3659
webmsgr <zapcontact000@gmail.com>
2021-11-09 19:24:10 -0800

    remove more unused

--- script.js
+++ script.js
@@ -650,11 +650,11 @@ function getPWADisplayMode() {
 function isPWA() {
     return getPWADisplayMode() != "browser";
 }
-
+/*
 var popoverTriggerList = [].slice.call(
     document.querySelectorAll('[data-bs-toggle="popover"]')
 );
-/*
+
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl);
 });

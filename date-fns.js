58661805ce58217cd80e67047e109f4edf73691a
Wackery <zapcontact000@gmail.com>
2021-12-09 16:02:58 +0000

    make script.js not a module
    now you can debug easier

---
+++ date-fns.js
@@ -0,0 +1,4 @@
+import { intervalToDuration, formatDuration, isAfter } from 'https://cdn.skypack.dev/date-fns' //TODO pin this so it doesnt change
+window.intervalToDuration = intervalToDuration
+window.formatDuration = formatDuration
+window.isAfter = isAfter

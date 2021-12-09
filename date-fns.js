e8b9f8754f2df4acd058ebc389eabab736231851
Wackery <32279125+webmsgr@users.noreply.github.com>
2021-12-09 08:25:55 -0800

    Pin date-fns dependency
    Nice

--- date-fns.js
+++ date-fns.js
@@ -1,4 +1,8 @@
-import { intervalToDuration, formatDuration, isAfter } from 'https://cdn.skypack.dev/date-fns' //TODO pin this so it doesnt change
+import { 
+  intervalToDuration, 
+  formatDuration, 
+  isAfter 
+} from 'https://cdn.skypack.dev/pin/date-fns@v2.27.0-kSwHFMsWRtnuspqyJxns/mode=imports,min/optimized/date-fns.js' 
 window.intervalToDuration = intervalToDuration
 window.formatDuration = formatDuration
 window.isAfter = isAfter

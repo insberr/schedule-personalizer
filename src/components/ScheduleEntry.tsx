c368b3300dcf9ed80b25b66394919e40cac3b1ca
Wackery <zapcontact000@gmail.com>
2022-05-03 15:38:03 +0000

    wow ok

--- src/components/ScheduleEntry.tsx
+++ src/components/ScheduleEntry.tsx
@@ -1,10 +1,10 @@
 function ScheduleEntry() {
     return (
     <div className="row justify-content-center">
-        <div className="col-3"> 00:00 - 99:99 </div>
-        <div className="col-3"> Life </div>
-        <div className="col-3"> Teacher </div>
-        <div className="col-3"> R Earth </div>
+        <div className="col"> 00:00 - 99:99 </div>
+        <div className="col"> Life </div>
+        <div className="col"> Teacher </div>
+        <div className="col"> R Earth </div>
     </div>
 )
 }
--- src/sw.ts
+++ src/sw.ts
@@ -1,9 +1,15 @@
 import { precacheAndRoute } from 'workbox-precaching';
+import {setCacheNameDetails} from 'workbox-core';
 
 type preCacheManifestEntry = {
     url: string;
     revision: string
 }
+
+setCacheNameDetails({
+    prefix: "schedule-personalizer",
+    suffix: "v2"
+})
 
 // @ts-expect-error cringe
 const manifest: preCacheManifestEntry[] = self.__precacheManifest

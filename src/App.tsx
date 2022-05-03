21a46287944598aa9517f334c01c1dc00d7e9453
Wackery <zapcontact000@gmail.com>
2022-05-03 20:33:19 +0000

    new saved data system

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React,  { useState } from "react"
 import SchedulePage from "./pages/schedule";
 import { Stdata } from "./types";
-import { data } from "./dataHandler";
+import { getSavedData } from "./dataHandler";
 
-console.log(data)
+//console.log(data)
 
 const SetupPage = React.lazy(() => import("./pages/setup"))
 //import { testData } from "./testData"; // prob should only import this when in development, to strip it out of production
 function App() {
-    const [sch, setSch] = useState<Stdata | undefined>(undefined) // maybe somehow load this from localstorage?
+    const [sch, setSch] = useState<Stdata | undefined>(getSavedData()) // maybe somehow load this from localstorage?
     if (!sch) {
         return <React.Suspense fallback={<div> loading... </div>}><SetupPage setSchedule={ setSch }/></React.Suspense> // replace loading text with a spinner
     }
--- src/dataHandler.ts
+++ src/dataHandler.ts
@@ -2,8 +2,20 @@ import { Stdata } from "./types";
 
 
 // get data from local storage
-export const data: Stdata  | { noData: true } = JSON.parse(localStorage.getItem("scheduleData") || JSON.stringify({ noData: true }))
+//export const data: Stdata  | { noData: true } = JSON.parse(localStorage.getItem("scheduleData") || JSON.stringify({ noData: true }))
 
+export function getSavedData(): Stdata | undefined {
+    const data = localStorage.getItem("scheduleData")
+    if (!data) {
+        // check for and migrate v1 data
+        return undefined
+    }
+    return JSON.parse(data)
+}
+
+export function saveData(data: Stdata) {
+    localStorage.setItem("scheduleData", JSON.stringify(data))
+}
 // translate it to the new format if its from v1
 
 // update schedules from studentvue if its been long enough

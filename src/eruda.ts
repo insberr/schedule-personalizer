c6892cc9ff0e378d2f0c73cd832934b95ec18466
insberr <jonahmatteson@gmail.com>
2022-05-03 13:12:26 -0700

    make lgtm alerts go away maybe ???

--- src/eruda.ts
+++ src/eruda.ts
@@ -1,7 +1,7 @@
 // eruda, will be stripped from production
 /// <reference path="eruda.d.ts"/>
 export default function eruda() {
-    if (process.env.NODE_ENV == "development" && window.location.href.includes("gitpod.io")) {
+    if (process.env.NODE_ENV == "development" && window.location.origin.includes("gitpod.io")) {
         import("eruda").then((s) => {s.init()})
         /*document.write('<script src="//cdn.jsdelivr.net/npm/eruda"></script>');
         document.write('<script>eruda.init();</script>');*/

ad66e1331c23f7169b5b63f9ba57e5ea44e265c6
webmsgr <zapcontact000@gmail.com>
2022-05-03 14:40:38 -0700

    move to map

--- src/pages/schedule/components/Schedule.tsx
+++ src/pages/schedule/components/Schedule.tsx
@@ -1,6 +1,7 @@
 import { Stdata } from "../../../types"
 import Center from "../../../components/Center"
 import ScheduleEntry from "./ScheduleEntry"
+import { getCurrentTerm } from "../../../lib"
 
 type ScheduleProps = {
     sch: Stdata
@@ -11,18 +12,14 @@ function Schedule(props: ScheduleProps) {
     // get current (viewed) trimester
     // get event and apply changes (if any)
     // make a value for todays schedule
-    const rows: JSX.Element[] = [];
-    for (const period of props.sch.terms[2].classes) {
-        rows.push(<ScheduleEntry key={period.period} period={period} />);
-    }
-
-    console.log(rows)
     
     return (<Center>
         <div className="row justify-content-center">
             <Center className="date">Monday: 69/69/69</Center>
         </div>
-        {rows}
+        { props.sch.terms[getCurrentTerm(props.sch)].classes.map((period) => {
+            return <ScheduleEntry key={period.period} period={period} />
+        }) }
     </Center>)
 }
 export default Schedule

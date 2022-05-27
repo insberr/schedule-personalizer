8d0750a21390c450aff9b56195c63c7743c66a36
Wackery <32279125+webmsgr@users.noreply.github.com>
2022-05-27 08:24:01 -0700

    Mmmm

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,7 +3,13 @@ import { getAnalytics } from 'firebase/analytics'
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig: FirebaseOptions = { // todo: copy the things
-    
+apiKey: "AIzaSyAPhi8PNcv-PJeu9wJ5qj8oCVj8c5bx3O0",
+authDomain: "schedule-personalizer-70ac7.firebaseapp.com",
+projectId: "schedule-personalizer-70ac7",
+storageBucket: "schedule-personalizer-70ac7.appspot.com",
+messagingSenderId: "982359110019",
+appId: "1:982359110019:web:1b60a09e7b9b42533dc6b6",
+measurementId: "G-0JFN98Q4NT"
 }
 
 const app = initializeApp(firebaseConfig);

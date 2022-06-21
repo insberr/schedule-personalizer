5e1623da37560655707be98d4fa2283826888594
Wackery <zapcontact000@gmail.com>
2022-06-21 15:53:37 +0000

    make the controls controlled
    controlled controls > uncontrolled controls

--- src/pages/setup/components/ManualClassEntry.tsx
+++ src/pages/setup/components/ManualClassEntry.tsx
@@ -7,24 +7,31 @@ import Form from 'react-bootstrap/Form'
 export function ManualClassEntry() {
     const id = useId()
     const [classname, setclassname] = useState("")
+    const [teacher, setteacher] = useState("")
+    const [room, setroom] = useState("")
+    function changeDo(setfunc: (v: string) => void) {
+        return (e: any) => { // use a better type
+            setfunc(e.currentTarget.value)
+        }
+    }
     // mayne convert the floatinglabel+control combo into a component
     return (
         <Container className="shader-bg mb-4 p-3 rounded">
-            Period name goes here (maybe with some styling)
+            Period name goes here (maybe with some styling) (id: {id})
             <Row>
                 <Form.Group as={Col}>
                     <Form.FloatingLabel controlId={id+"classname"} label="Class Name"> 
-                        <Form.Control type="text" placeholder="Class Name" />
+                        <Form.Control type="text" onChange={changeDo(setclassname)} value={classname} placeholder="Class Name" />
                     </Form.FloatingLabel>
                 </Form.Group>
                 <Form.Group as={Col}>
                     <Form.FloatingLabel controlId={id+"tname"} label="Teacher Name">
-                        <Form.Control placeholder="Teacher Name" />
+                        <Form.Control value={teacher} onChange={changeDo(setteacher)} placeholder="Teacher Name" />
                     </Form.FloatingLabel>
                 </Form.Group>
                 <Form.Group as={Col}>
                     <Form.FloatingLabel controlId={id+"room"} label="Room Number">
-                        <Form.Control placeholder="Room Number" />
+                        <Form.Control value={room} onChange={changeDo(setroom)} placeholder="Room Number" />
                     </Form.FloatingLabel>
                 </Form.Group>
             </Row>

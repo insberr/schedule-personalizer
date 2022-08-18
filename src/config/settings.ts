a0aaa480a5b57fdb4cd173e62c5f3144161eaef2
insberr <jonahmatteson@gmail.com>
2022-08-17 18:03:32 -0700

    settings file stuff

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -6,3 +6,10 @@ export const numberOfPeriods= 6;
 
 // The usual period lunch is based on.
 export const normalLunchBasedOnPeriod = 3;
+
+// Settings To Implement
+
+// StudentVue API URL (You can host it yourself (WE SHOULD PUBLISH THE SERVER CODE OR SOMETHING IDK))
+// Do Not Add Trailing Slash (/), else the code will error
+//     (I should make sure the code auto detects a trailing slash, this is just a reminder)
+export const studentVueApiURL = 'https://studentvue.wackery.com';

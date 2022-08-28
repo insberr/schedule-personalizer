cee9b05b5fb34310593286511a90d921c3efa934
insberr <jonahmatteson@gmail.com>
2022-08-28 14:29:52 -0700

    test i guess

--- src/test/testApp.tsx
+++ src/test/testApp.tsx
@@ -1,29 +1,42 @@
 import { useEffect, useRef, useState } from "react";
 import { usePinchZoom } from "react-use";
+import Center from "../components/Center";
 
 export const Demo = () => {
-    const [scale, setState] = useState(1);
-    const scaleRef = useRef();
+    const [scale, setState] = useState(0);
+    const scaleRef = useRef<HTMLDivElement>(null);
     const { zoomingState, pinchState } = usePinchZoom(scaleRef);
 
     useEffect(() => {
+        
         if (zoomingState === "ZOOM_IN") {
-        // perform zoom in scaling
-        setState(scale + 0.1)
+            // perform zoom in scaling
+            setState(scale + 0.1)
         } else if (zoomingState === "ZOOM_OUT") {
-        // perform zoom out in scaling
-        setState(scale - 0.1)
+            // perform zoom out in scaling
+            setState(scale - 0.1)
         }
-    }, [zoomingState]);
+    }, [zoomingState, pinchState]);
 
     return (
-        <div ref={scaleRef}>
-        <img
-            src="https://www.olympus-imaging.co.in/content/000107506.jpg"
-            style={{
-            zoom: scale,
-            }}
-        />
-        </div>
+        <>
+        <div>test page</div>
+        <pre className="paper">
+            Pinch: { JSON.stringify(pinchState, null, 2) }
+            { '\n' }
+            Zoom: { JSON.stringify(zoomingState, null, 2) }
+        </pre>
+        <Center>
+            <div ref={scaleRef}>
+                <img
+                    src="https://www.olympus-imaging.co.in/content/000107506.jpg"
+                    style={{
+                        'width': '80%', 'height': '80%',
+                        zoom: scale,
+                    }}
+                />
+            </div>
+        </Center>
+        </>
     )
 }

a4f529638c962a92685228c71ab17bab53aab850
insberr <jonahmatteson@gmail.com>
2022-08-28 13:59:06 -0700

    uh force update?

--- src/sw.ts
+++ src/sw.ts
@@ -15,3 +15,27 @@ setCacheNameDetails({
 const manifest: preCacheManifestEntry[] = self.__precacheManifest
 
 precacheAndRoute(manifest); // yoo caching
+
+self.addEventListener('message', (event) => {   
+    const msg = event.data;
+    console.log("SW Received Message: " + msg);
+        if (msg === 'clearCache') {
+            console.log('Clearing Workbox Cache.');
+            refreshCacheAndReload();
+    }
+});
+const registration = new ServiceWorkerRegistration();
+const refreshCacheAndReload = () => {
+    if ('serviceWorker' in navigator) {
+      registration.unregister();
+      caches.keys().then(cacheNames => {
+        cacheNames.forEach(cacheName => {
+          caches.delete(cacheName);
+        });
+      }).then(() => {
+        registration.update();
+      })
+    }
+    console.log('serviceWorker' in navigator)
+    setTimeout(function () { window.location.replace(""); }, 300)
+}

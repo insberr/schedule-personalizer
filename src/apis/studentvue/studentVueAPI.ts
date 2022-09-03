7cd45b205d7d6d3c723cf2696be09198726a8cfb
insberr <jonahmatteson@gmail.com>
2022-09-03 10:44:24 -0700

    forgot a return

--- src/apis/studentvue/studentVueAPI.ts
+++ src/apis/studentvue/studentVueAPI.ts
@@ -4,6 +4,215 @@ import { courseTitleNameCase, redactStudentInfo, toTitleCase } from "../../lib";
 import { StudentInfo, StudentClassList, validate, isError, StudentSchoolInfo } from "./api"
 import * as Sentry from '@sentry/react';
 
+const fakeData = {
+    "code": "SUCCESS",
+    "content": {
+     "TodayScheduleInfoData": {
+      "SchoolInfos": {}
+     },
+     "ClassLists": [
+      [
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "1",
+        "CourseTitle": "WIND ENSEMBLE A",
+        "RoomName": "124 BAND",
+        "Teacher": "RYAN WHITEHEAD",
+        "TeacherEmail": "RWHITEHEAD@bethelsd.org",
+        "SectionGU": "5FC6A372-CEDC-410F-AD67-F3428F672939",
+        "TeacherStaffGU": "961F3156-7FD9-40E9-A296-651C27259733"
+       },
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "2",
+        "CourseTitle": "PRE CALCULUS A",
+        "RoomName": "201",
+        "Teacher": "RACHEL STROM",
+        "TeacherEmail": "RSTROM@BETHELSD.ORG",
+        "SectionGU": "E2308477-1BB9-42A6-95F9-EA0167B68994",
+        "TeacherStaffGU": "9B795071-1EAF-492B-A84B-209DE77148BC"
+       },
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "3",
+        "CourseTitle": "ECONOMICS",
+        "RoomName": "P7",
+        "Teacher": "KEVIN SCHWOCH",
+        "TeacherEmail": "KSCHWOCH@bethelsd.org",
+        "SectionGU": "7880BD3A-3B6D-48D5-B671-2700AC6D3A74",
+        "TeacherStaffGU": "554D6D51-BA62-414B-AE85-4359EECBEBAD"
+       },
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "4",
+        "CourseTitle": "CYBERSECURITY A",
+        "RoomName": "219",
+        "Teacher": "ZESTY FERNANDEZ",
+        "TeacherEmail": "ZFERNANDEZ@bethelsd.org",
+        "SectionGU": "B51C5579-1A04-4548-AA09-7933B1541B69",
+        "TeacherStaffGU": "918E6D22-B9A4-433C-AABF-A9B89FF5B6EF"
+       },
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "5",
+        "CourseTitle": "AP ENG LIT A",
+        "RoomName": "118",
+        "Teacher": "SABRINA SORIERO",
+        "TeacherEmail": "SSORIERO@bethelsd.org",
+        "SectionGU": "5C65CD24-B293-485A-893F-3206C33C0F3C",
+        "TeacherStaffGU": "BE947D94-A04C-425F-B91F-5AA4402F312F"
+       },
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "8",
+        "CourseTitle": "ADVISORY 12",
+        "RoomName": "120",
+        "Teacher": "JOHN STUMPF",
+        "TeacherEmail": "JSTUMPF@BETHELSD.ORG",
+        "SectionGU": "ADEA4167-C1F8-4ABE-A3FF-DB2B90F9D21A",
+        "TeacherStaffGU": "CDE89AD0-1D63-4DB7-8C89-A66F1C5C41E2"
+       }
+      ],
+      [
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "1",
+        "CourseTitle": "WIND ENSEMBLE B",
+        "RoomName": "",
+        "Teacher": "RYAN WHITEHEAD",
+        "TeacherEmail": "RWHITEHEAD@bethelsd.org",
+        "SectionGU": "CC658524-83D4-475F-85C8-9438C9CF2831",
+        "TeacherStaffGU": "961F3156-7FD9-40E9-A296-651C27259733"
+       },
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "2",
+        "CourseTitle": "VIDEO PRODUCTION 1",
+        "RoomName": "115",
+        "Teacher": "DENISE GIBBS",
+        "TeacherEmail": "DGIBBS@bethelsd.org",
+        "SectionGU": "85D980F1-1B8A-4EAD-919E-22591D29E2B5",
+        "TeacherStaffGU": "05B18ACF-4FB0-4D48-A97B-5E8A295FE753"
+       },
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "3",
+        "CourseTitle": "INTRO TO PHY FIT TECH",
+        "RoomName": "B BLDG",
+        "Teacher": "WHITNEY HUBEEK",
+        "TeacherEmail": "WHUBEEK@BETHELSD.ORG",
+        "SectionGU": "2DF00EF7-4E3D-4648-8453-947A6DF8C7F0",
+        "TeacherStaffGU": "1B5BFD83-2919-4A21-85A0-CD82D6364AF2"
+       },
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "4",
+        "CourseTitle": "CYBERSECURITY B",
+        "RoomName": "",
+        "Teacher": "ZESTY FERNANDEZ",
+        "TeacherEmail": "ZFERNANDEZ@bethelsd.org",
+        "SectionGU": "F574F33C-18DC-4022-ACE8-8A51CF0420FF",
+        "TeacherStaffGU": "918E6D22-B9A4-433C-AABF-A9B89FF5B6EF"
+       },
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "5",
+        "CourseTitle": "AP ENG LIT B",
+        "RoomName": "",
+        "Teacher": "SABRINA SORIERO",
+        "TeacherEmail": "SSORIERO@bethelsd.org",
+        "SectionGU": "2E1576CB-E919-4D15-8D36-BB5E64057DFE",
+        "TeacherStaffGU": "BE947D94-A04C-425F-B91F-5AA4402F312F"
+       },
+       {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "8",
+        "CourseTitle": "ADVISORY 12",
+        "RoomName": "120",
+        "Teacher": "JOHN STUMPF",
+        "TeacherEmail": "JSTUMPF@BETHELSD.ORG",
+        "SectionGU": "ADEA4167-C1F8-4ABE-A3FF-DB2B90F9D21A",
+        "TeacherStaffGU": "CDE89AD0-1D63-4DB7-8C89-A66F1C5C41E2"
+       }
+      ],
+        {
+        "AdditionalStaffInformationXMLs": {},
+        "Period": "1",
+        "CourseTitle": "WIND ENSEMBLE C",
+        "RoomName": "",
+        "Teacher": "RYAN WHITEHEAD",
+        "TeacherEmail": "RWHITEHEAD@bethelsd.org",
+        "SectionGU": "1F39F7A5-7486-4C37-ADA6-8E02544089BC",
+        "TeacherStaffGU": "961F3156-7FD9-40E9-A296-651C27259733"
+       }
+     ],
+     "TermLists": {
+      "TermListing": [
+       {
+        "TermDefCodes": {
+         "TermDefCode": [
+          {
+           "TermDefName": "T1H"
+          },
+          {
+           "TermDefName": "YRH"
+          }
+         ]
+        },
+        "TermIndex": "0",
+        "TermCode": "1",
+        "TermName": "Trimester 1",
+        "BeginDate": "09/06/2022",
+        "EndDate": "12/06/2022",
+        "SchoolYearTrmCodeGU": "A00216D4-8B51-418F-B184-E8453A16593E"
+       },
+       {
+        "TermDefCodes": {
+         "TermDefCode": [
+          {
+           "TermDefName": "T2H"
+          },
+          {
+           "TermDefName": "YRH"
+          }
+         ]
+        },
+        "TermIndex": "1",
+        "TermCode": "2",
+        "TermName": "Trimester 2",
+        "BeginDate": "12/08/2022",
+        "EndDate": "03/27/2023",
+        "SchoolYearTrmCodeGU": "4B53AD0C-A7C0-4138-A03D-2C7A5580DEA0"
+       },
+       {
+        "TermDefCodes": {
+         "TermDefCode": [
+          {
+           "TermDefName": "T3H"
+          },
+          {
+           "TermDefName": "YRH"
+          }
+         ]
+        },
+        "TermIndex": "2",
+        "TermCode": "3",
+        "TermName": "Trimester 3",
+        "BeginDate": "03/28/2023",
+        "EndDate": "06/23/2023",
+        "SchoolYearTrmCodeGU": "9570BAFB-5132-49B7-8F99-E2797E251986"
+       }
+      ]
+     },
+     "ConcurrentSchoolStudentClassSchedules": {},
+     "xmlns:xsd": "http://www.w3.org/2001/XMLSchema",
+     "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+     "TermIndex": "0",
+     "TermIndexName": "Trimester 1",
+     "ErrorMessage": "",
+     "IncludeAdditionalStaffWhenEmailingTeachers": "true"
+    }
+   }
 export async function validateCredentials(username: string, password: string): Promise<boolean> {
     return await validate(username, password); // mm
 }
@@ -60,7 +269,7 @@ export type StudentVueAPIDataUserDate = {
 }
 
 export function convertStudentvueDataToTerms(data: StudentVueAPIData): Terms {
-    const studentvueTerms = data.content.ClassLists;
+    const studentvueTerms = fakeData.content.ClassLists // data.content.ClassLists;
     console.log('studentVueTerms.length: ', studentvueTerms.length);
 
     const newTerms = settings.termsDates.map(t => {
@@ -95,7 +304,7 @@ export function convertStudentvueDataToTerms(data: StudentVueAPIData): Terms {
                     id: c.TeacherStaffGU
                 }
             }]
-
+            return t;
         }
         t.classes = (studentvueTerms[i] as StudentVueAPIDataClassListsTermClass[]).map(c => {
             console.log('studentVueTerms.map => (c): ', c)

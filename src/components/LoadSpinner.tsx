177d0a7a2c739e6b32603c248328f7ad5eb4275b
Jonah Matteson <jonahmatteson@gmail.com>
2022-09-20 21:26:40 -0700

    added attempt to reset if failed to load

--- src/components/LoadSpinner.tsx
+++ src/components/LoadSpinner.tsx
@@ -1,7 +1,7 @@
 import Spinner from "react-bootstrap/Spinner";
 
 function LoadSpinner() {
-    return <Spinner animation="grow" className="full-center-spinner "/>
+    return <Spinner animation="grow" className="full-center-spinner text-crimson"/>
 }
 
 export default LoadSpinner;

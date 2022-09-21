2f8dede42cec48a7845864d01e7631d9ff35446b
Wackery <zapcontact000@gmail.com>
2022-09-21 18:31:40 +0000

    i hate the anti-christ

--- src/sw.ts
+++ src/sw.ts
@@ -6,19 +6,31 @@ import {registerRoute} from 'workbox-routing';
 import {CacheFirst, NetworkFirst} from 'workbox-strategies';
 //import { ExpirationPlugin }  from "workbox-expiration";
 //import { request } from "http";
+import {CacheableResponsePlugin} from 'workbox-cacheable-response'
 
 self.addEventListener("install", () => {
     self.skipWaiting().then(clientsClaim);
-    console.log("oh shit guess who it fuckin is, its the gamer service worker here with offline support.")
+    console.log("oh shit guess who it fuckin is, its the gamer service worker here with offline support. ("+version+")")
 })
+self.addEventListener("activate", () => {
+    caches.keys().then(cacheNames => {
+        cacheNames.forEach(cacheName => {
+            if (cacheName.split("$")[1] != version) {
+                console.log("deleting old cache "+cacheName)
+                caches.delete(cacheName);
+            }
+        });
+      });
+})
+
 const toNetwork = [
     "document",
     "manifest"
 ]
 
 const opt = {
-    plugins: [new BackgroundSyncPlugin("syncy")],
-    "cacheName": "schedulecache"
+    plugins: [new BackgroundSyncPlugin("syncy"), new CacheableResponsePlugin({statuses: [0, 200]}),],
+    "cacheName": "schedulecache$"+version+"$"
 }
 registerRoute((r) => toNetwork.includes(r.request.destination), new NetworkFirst(opt))
 registerRoute((r) => !toNetwork.includes(r.request.destination)&&new URL(r.request.url).origin==location.origin,  new CacheFirst(opt))

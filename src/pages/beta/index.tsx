707dd17d40208c14435b19c8b2170ad029bc9905
Jonah Matteson <jonahmatteson@gmail.com>
2022-10-04 18:06:15 -0700

    map button ig

--- src/pages/beta/index.tsx
+++ src/pages/beta/index.tsx
@@ -3,8 +3,10 @@ import { useNavigate } from "../../router/hooks";
 import { Page } from "../../storage/page";
 
 export function BetaMap() {
+    const navigate = useNavigate();
+    
     return (<>
-        <Button onClick={() => { navigator(Page.SCHEDULE) }}>Back to schedule</Button>
+        <Button onClick={() => { navigate(Page.SCHEDULE) }}>Back to schedule</Button>
         <div>Haha you thought the new map would actually be done? And the paths? Oh yeah those are causing me brain damage.</div>
         <div>Ive haven&apos;t been doing any of my homework because these paths ignored walls for the longest time.</div>
         <iframe src="https://astary.insberr.com/maptest/maptest" width="50%" height="400" frameBorder="0"></iframe>

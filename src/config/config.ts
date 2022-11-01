d0166f3924984d49a8008b318ce3d5cb07a30614
Jonah Matteson <jonahmatteson@gmail.com>
2022-11-01 11:08:38 -0700

    ok

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,6 @@
 
 // Sentry Error Reporting
 // Sentry.io DSN if you want to use it
-const sentryDSN = 'https://a5ab5a1946bd4e31a06ca456fc5b30fc@o1233680.ingest.sentry.io/6382608';
+export const sentryDSN = 'https://a5ab5a1946bd4e31a06ca456fc5b30fc@o1233680.ingest.sentry.io/6382608';
 
 // const Schools = ...;

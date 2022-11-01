63193404dd99390ba1602b41b72055ab452a8afe
Jonah Matteson <jonahmatteson@gmail.com>
2022-11-01 11:08:01 -0700

    broken obviously

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,27 +1,7 @@
 // Main config
-// Uh
-// This is so we can add multi-school support
-// Someday we will add multi-school support
 
-// If the config is installed as a module from a git repo, uncomment this and comment out everything else
-/*
-import * as config from 'configModuleName';
-export default config;
-*/
+// Sentry Error Reporting
+// Sentry.io DSN if you want to use it
+const sentryDSN = 'https://a5ab5a1946bd4e31a06ca456fc5b30fc@o1233680.ingest.sentry.io/6382608';
 
-// All or part of config is stored on a database?
-// This is recommended for the events and schedules config, and any other config that needs to be updated frequently
-// and quickly updated for users. A web request is faster than waiting for the browser to decide to
-// update the cache
-/*
-// Write code for that here
-*/
-
-// Multi School (Not yet implemented)
-/*
-import * as schoolName from './schoolName';
-export const multiSchool = true;
-export const schoolConfigs = {
-    'School Name': schoolName
-};
-*/
+// const Schools = ...;

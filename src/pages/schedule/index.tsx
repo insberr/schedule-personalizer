4d11eb8a6d1faa8b8d4a520ca3a13302307e09ca
webmsgr <zapcontact000@gmail.com>
2022-11-18 10:05:56 -0800

    add exec to preview
    and so it begins

--- src/pages/schedule/index.tsx
+++ src/pages/schedule/index.tsx
@@ -1,6 +1,11 @@
 import { store } from '../../storage';
 
 export function SchedulePage() {
-    return <pre> {store.scs.value?.pretty()} </pre>;
+    return (
+        <>
+            <pre> {store.scs.value?.pretty()} </pre>
+            <pre> {JSON.stringify(store.scs.value?.exec(), null, 4)} </pre>
+        </>
+    );
     // you definatly shouldnt use the SCS value directly, we should compute the users full merged schedule and use that
 }

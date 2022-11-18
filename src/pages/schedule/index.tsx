1146a4cfd7c843c70929d00f6058e427c6ddf8f1
webmsgr <zapcontact000@gmail.com>
2022-11-18 08:21:34 -0800

    add note about using SCS

--- src/pages/schedule/index.tsx
+++ src/pages/schedule/index.tsx
@@ -2,4 +2,5 @@ import { store } from '../../storage';
 
 export function SchedulePage() {
     return <pre> {store.scs.value?.pretty()} </pre>;
+    // you definatly shouldnt use the SCS value directly, we should compute the users full merged schedule and use that
 }

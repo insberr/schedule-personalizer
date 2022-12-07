6271bf46874c8dd50baf48f9935f3adbbe51a3e4
Jonah Matteson <jonahmatteson@gmail.com>
2022-12-06 18:01:26 -0800

    pushing

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,8 +1,8 @@
 mod utils;
 
+use js_sys;
 use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
-
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -15,7 +15,7 @@ pub fn init() {
 }
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
@@ -27,4 +27,29 @@ pub fn greet() {
 #[wasm_bindgen]
 pub fn funny_number() -> u32 {
     69
+}
+
+#[wasm_bindgen]
+pub fn get_schedule_for_display(user: String) -> String {
+    return (user + "not implemented yet").to_string();
+}
+
+// I spent way too long on getting this to work...
+#[wasm_bindgen]
+pub fn test_js_obj(obj: JsValue) -> Result<String, JsValue> {
+    let value: Result<JsValue, JsValue> = js_sys::Reflect::get(&obj, &JsValue::from_str("name"));
+    if value.is_err() {
+        return Err(value.err().unwrap());
+    }
+    let v: String = value.unwrap().as_string().unwrap();
+    Ok(v)
+}
+
+#[wasm_bindgen]
+pub struct TestObj {
+    pub name: &str,
+}
+#[wasm_bindgen]
+pub fn test_struct_obj(obj: TestObj) -> String {
+    return obj.name.to_string();
 }
--- src/pages/schedule/index.tsx
+++ src/pages/schedule/index.tsx
@@ -4,7 +4,7 @@ import { store } from '../../storage';
 import { Schedule } from '../../components/schedule';
 import { JSONTree } from 'react-json-tree';
 import { SCS } from 'schedule-script';
-import { funny_number } from 'backend';
+import { funny_number, get_schedule_for_display, test_js_obj } from 'backend';
 
 export function SchedulePage() {
     return (
@@ -52,9 +52,9 @@ export function SchedulePage() {
                     },
                 ]}
             />
-            <p>
-                The funny number from rust: '{funny_number()}'
-            </p>
+            <p>The funny number from rust: &apos;{funny_number()}&apos;</p>
+            <p>{get_schedule_for_display('test')}</p>
+            <p>{test_js_obj({ name: 'hello' })}</p>
         </>
     );
     // you definatly shouldnt use the SCS value directly, we should compute the users full merged schedule and use that

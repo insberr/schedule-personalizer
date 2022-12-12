e5307a29c215841d00ac40589fc300c15e3b19d0
Wackery <zapcontact000@gmail.com>
2022-12-12 17:00:07 +0000

    ok

--- rollup-plugin-scs/index.ts
+++ rollup-plugin-scs/index.ts
@@ -1,8 +1,25 @@
 import { loopWhile } from "deasync"
+import { basename } from "path";
 import { SCS } from "schedule-script";
-
+type EmittedChunk = {
+    type: 'chunk';
+    id: string;
+    name?: string;
+    fileName?: string;
+    implicitlyLoadedAfterOneOf?: string[];
+    importer?: string;
+    preserveSignature?: 'strict' | 'allow-extension' | 'exports-only' | false;
+  };
+  
+  type EmittedAsset = {
+    type: 'asset';
+    name?: string;
+    fileName?: string;
+    source?: string | Uint8Array;
+  };
 type rollupContext = {
     resolve: (source: string, importer?: string, options?: {skipSelf?: boolean, isEntry?: boolean, assertions?: {[key: string]: string}, custom?: {[plugin: string]: any}}) => Promise<{id: string, external: boolean | "absolute", assertions: {[key: string]: string}, meta: {[plugin: string]: any} | null, moduleSideEffects: boolean | "no-treeshake", syntheticNamedExports: boolean | string}>
+    emitFile:  (emittedFile: EmittedChunk | EmittedAsset) => string
 }
 
 export function scs(this: rollupContext) {
@@ -19,14 +36,19 @@ export function scs(this: rollupContext) {
                 let out: string | null = null;
                 this.resolve(iid).then((v) => out = v.id)
                 loopWhile(() => out != null) // fuck shit
-                // @ts-ignore
+                // @ts-expect-error you cant expect god to do all the work
                 return out as string
             }
             if (!id.endsWith(".scs")) {
                 return;
             }
-            let s = new SCS(code, resolver)
-            return "export default '"+s.bundle()+"';"
+            const s = new SCS(code, resolver)
+            const emited = this.emitFile({
+                type: "asset",
+                fileName: basename(id),
+                source: s.bundle()
+            })
+            return `export default import.meta.ROLLUP_FILE_URL_${emited};`;
         }
 
     }

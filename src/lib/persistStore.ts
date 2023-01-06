d712e80ee8b4db224ff4d81fd0f39943e9b612dc
Wackery <zapcontact000@gmail.com>
2023-01-06 18:06:53 +0000

    serializer and deserializer

--- src/lib/persistStore.ts
+++ src/lib/persistStore.ts
@@ -1,16 +1,16 @@
 //mport { json } from "@sveltejs/kit"
-
+import { serialize, deserialize } from "@macfja/serializer";
 import { writable, type Writable } from "svelte/store";
 
 export function persistWritable<T>(key: string, defaultItem: T): Writable<T> {
     let dt = localStorage.getItem(key)
     if (dt == undefined) {
-        dt = JSON.stringify({v: defaultItem});
+        dt = serialize({v: defaultItem});
     }
-    let obj: T = JSON.parse(dt).v;
+    let obj: T = deserialize(dt).v;
     const store = writable(obj);
     store.subscribe((n) => {
-        localStorage.setItem(key, JSON.stringify({v: n}))
+        localStorage.setItem(key, serialize({v: n}))
     })
     return store;
 }

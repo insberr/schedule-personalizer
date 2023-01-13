4bebc5e7e49d40320e75edebed6e5858de509e3f
webmsgr <zapcontact000@gmail.com>
2023-01-13 12:04:17 -0800

    make the admin page a bit better

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,1 +1,5 @@
-<a class="link hover:link-secondary" href="/admin/editor">Editor</a>
+<div class="center bg-base-200 rounded p-5" style="margin-top: 5em">
+    <h2>Schedule Admin</h2>
+    <h4>You probably shouldn't be here</h4>
+    <a class="link hover:link-secondary" href="/admin/editor">Editor</a>
+</div>

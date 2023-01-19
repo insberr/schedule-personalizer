ad9f2b89746d099753ae1608e85c08b351f30e23
webmsgr <zapcontact000@gmail.com>
2023-01-18 18:44:38 -0800

    fix choppy animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,9 +19,13 @@
 <button class="btn" on:click={() => (d = addDays(d, -1))}>
     &lt;-
 </button><button class="btn" on:click={() => (d = addDays(d, 1))}> -> </button>
-<div class="center grid grid-cols-1 grid-rows-auto" use:autoAnimate>
+<div
+    class="m-auto flex flex-col h-fit text-center"
+    style="width:75vw"
+    use:autoAnimate
+>
     {#each hydrated.schedule as cls (cls.name + cls.period + cls.classID)}
-        <div class="grid grid-rows-1 grid-cols-4">
+        <div class="flex-none grid grid-rows-1 grid-cols-4">
             <div>{cls.start} -> {cls.end}</div>
             <div>{cls.name}</div>
             <div>{cls.teacher.name}</div>

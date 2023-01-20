e89ab7c2e3a71426ee119eb482f9c1dfb09c6385
webmsgr <zapcontact000@gmail.com>
2023-01-19 19:16:21 -0800

    worknstuff

--- src/lib/components/ScheduleRow.svelte
+++ src/lib/components/ScheduleRow.svelte
@@ -1,11 +1,16 @@
 <script type="ts">
     import type { DisplayCL } from '$types';
     import autoAnimate from '@formkit/auto-animate';
-    import { format, isAfter, parse } from 'date-fns';
+    import { endOfDecade, format, isAfter, parse } from 'date-fns';
     import { onMount } from 'svelte';
     import { slide } from 'svelte/transition';
     import Fa from 'svelte-fa';
-    import { faSun } from '@fortawesome/free-solid-svg-icons';
+    import {
+        faCircle,
+        faMinus,
+        faPlus,
+        faSun,
+    } from '@fortawesome/free-solid-svg-icons';
     let hide = true;
     export let displayDate: Date;
     export let cls: DisplayCL;
@@ -18,6 +23,7 @@
             clearInterval(x);
         };
     });
+
     $: startTime = parse(cls.start, 'HH:mm', displayDate);
     $: endTime = parse(cls.end, 'HH:mm', displayDate);
     $: isNow = isAfter(currentDate, startTime) && isAfter(endTime, currentDate);
@@ -29,17 +35,34 @@
     }}
     on:keydown
 >
-    <div
-        class="w-full h-fit grid max-md:grid-cols-3 gap-2 px-1 text-center grid-cols-4 grid-rows-1 place-content-center justify-around justify-items-center"
-    >
-        <div>{format(startTime, 'h:mm')} - {format(endTime, 'h:mm')}</div>
-        <div>{cls.name}</div>
-        <div class="max-md:hidden">{cls.teacher.name}</div>
-        <div class:hidden={cls.room == ''}>R{cls.room}</div>
+    <div class="flex flex-row justify-evenly items-center">
+        <div class="px-2" />
+        <Fa icon={faCircle} class={!isNow ? 'invisible' : ''} />
+        <div
+            class="w-full h-fit grid max-md:grid-cols-2 gap-2 px-1 text-center grid-cols-4 grid-rows-1 place-content-center justify-around justify-items-center"
+        >
+            <div>{format(startTime, 'h:mm')} - {format(endTime, 'h:mm')}</div>
+            <div>{cls.name}</div>
+            <div class="max-md:hidden">{cls.teacher.name}</div>
+            <div class="max-md:hidden" class:hidden={cls.room == ''}>
+                R{cls.room}
+            </div>
+        </div>
+        <Fa icon={hide ? faPlus : faMinus} />
+        <div class="px-2" />
     </div>
     {#if !hide}
         <div transition:slide|local class="bottom block">
-            {isNow ? 'Now!' : 'Not now!'}
+            {#if isNow}
+                <!-- During class -->
+                {cls.name} ends in 420 minutes, 69000 seconds
+            {:else if isAfter(currentDate, endTime)}
+                <!-- After Class -->
+                Class Ended
+            {:else}
+                <!-- Before Class -->
+                {cls.name} begins in 420 minutes, 69000 seconds
+            {/if}
         </div>
     {/if}
 </div>

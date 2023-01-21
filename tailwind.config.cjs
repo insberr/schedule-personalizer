7e0231fef9974c89941a7dc8966c87da05a8f995
Steam Deck User <zapcontact000@gmail.com>
2023-01-21 14:47:49 -0800

    color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,49 @@ const config = {
   plugins: [daisyui],
   daisyui: {
     themes: ["dark", "light"],
+    themes: [
+      {
+        dark: {
+        
+
+
+"primary": "#DC143C",
+        
+
+
+"secondary": "#D926AA",
+        
+
+
+"accent": "#1FB2A5",
+        
+
+
+"neutral": "#191D24",
+        
+
+
+"base-100": "#2A303C",
+        
+
+
+"info": "#3ABFF8",
+        
+
+
+"success": "#36D399",
+        
+
+
+"warning": "#FBBD23",
+        
+
+
+"error": "#F87272",
+        },
+      },
+    ],
+
     darkTheme: "dark" // sets the dark theme for dark mode users
   }
 };

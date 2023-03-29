14641ab4a80312542bc26c4a79645a8bec0a2076
Jonah Matteson <jonahmatteson@gmail.com>
2023-03-29 14:38:57 -0700

    what did I do to

--- src/App.tsx
+++ src/App.tsx
@@ -46,9 +46,13 @@ function App() {
                     return undefined;
                 });
 
+            console.log('StudentVue Credentials Validation; Is Valid: ' + isValid);
             if (isValid === true || isValid === undefined) {
                 return;
             }
+
+            // Create an alert to tell the user that they need to login again
+
             dispatch(
                 setStudentVueData({
                     password: stv.password,
